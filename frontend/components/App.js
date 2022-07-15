1fa53d47dc6b171348ec141654cc079f41d0e7f9
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-07-15 00:27:51 -0400

    set component did mount to fetch from api,set todo state, displa todo

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+  componentDidMount(){
+    fetch(URL)
+    .then(res => res.json())
+    .then(todos=> this.setState({...this.state, todos: todos.data}))
+  }
   render() {
-    return null
+    return (<>
+         {this.state.todos.map(todos => <p key={todos.id}>{todos.name}</p>)}
+    </>);
   }
 }
